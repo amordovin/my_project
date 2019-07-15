58e66599bbd135853a6eb8056d6ff212e5b2b216
Alexander Mordovin <aa.mordovin@gmail.com>
2019-07-15 16:45:50 +0400

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -13,8 +13,8 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-         assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Нет формы логина"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Нет формы логина"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-       assert self.is_element_present(*LoginPageLocators.REGISTER_LINK), "Нет формы регистрации"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_LINK), "Нет формы регистрации"
